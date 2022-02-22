75c7faa49e8c63d29d3c96e77a5eedfa47f4034a
albgom21 <albgom21@ucm.es>
2022-02-22 09:48:51 +0100

    Compilación de Ogre
    Co-Authored-By: simekons <62706897+simekons@users.noreply.github.com>
    Co-Authored-By: mamigu05 <62902566+mamigu05@users.noreply.github.com>
    Co-Authored-By: David Carmona Fauste <37439085+DavidCarmonaFauste@users.noreply.github.com>
    Co-Authored-By: Agusmoure <48672414+Agusmoure@users.noreply.github.com>

--- VERNIERENGINE/Src/RenderEngine/RenderMain.cpp
+++ VERNIERENGINE/Src/RenderEngine/RenderMain.cpp
@@ -1,9 +1,25 @@
-// RenderEngine.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// ENGINE.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-#include "RenderEngine.h"
-#include <iostream>
 
-void RenderEngine::Saluda(const char* nombre)
+#include <iostream>
+#include "RenderEngine.h"
+#include <OgreRoot.h>
+
+int main()
 {
-	std::cout << "Hola " << nombre;
+	Ogre::Root* root;
+	root = new Ogre::Root();
+	std::cout << "Hola\n";
+	//RenderEngine::Saluda("Pedro Pablo");
+	return 0;
 }
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
