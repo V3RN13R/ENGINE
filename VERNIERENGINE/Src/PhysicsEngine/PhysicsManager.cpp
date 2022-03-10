10e5ac54d485ea1ce6f369c58c1ec9797cf9badc
madelino22 <madelino@ucm.es>
2022-03-10 19:10:58 +0100

    Añadidos métodos en PhysicsManager
    Co-Authored-By: davidrodriguezz <62748674+davidrodriguezz@users.noreply.github.com>
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>

--- VERNIERENGINE/Src/PhysicsEngine/PhysicsManager.cpp
+++ VERNIERENGINE/Src/PhysicsEngine/PhysicsManager.cpp
@@ -1,58 +1,58 @@
-//#include "PhysicsManager.h"
-//
-//
-//#include <btBulletCollisionCommon.h>
-//#include <btBulletDynamicsCommon.h>
-//
-////#include "Vector3D.h"
-//
-//PhysicsManager* PhysicsManager::_instance = nullptr;
-//PhysicsManager* PhysicsManager::getInstance()
-//{
-//	return _instance;
-//}
-//
-//bool PhysicsManager::setUpInstance() {
-//	if (_instance == nullptr) {
-//		try {
-//			_instance = new PhysicsManager();
-//			_instance->init(Vector3(0.0, -9.8, 0.0));
-//		}
-//		catch (...) {
-//			return false;
-//		}
-//	}
-//
-//	return true;
-//}
-//
-//PhysicsManager::PhysicsManager(){
-//	//registerComponent("RigidBody", 0, []() -> RigidBody * { return new RigidBody(); });
-//
-//}
-//PhysicsManager::~PhysicsManager()
-//{
-//}
-//;
-//
-//void PhysicsManager::init(const Vector3 gravity)
-//{
-//	collConfig = new btDefaultCollisionConfiguration();
-//
-//	collDispatcher = new btCollisionDispatcher(collConfig);
-//
-//	broadPhaseInterface = new btDbvtBroadphase();
-//	constraintSolver = new btSequentialImpulseConstraintSolver();
-//
-//	dynamicsWorld = new btDiscreteDynamicsWorld(collDispatcher, broadPhaseInterface,
-//		constraintSolver, collConfig);
-//
-//	dynamicsWorld->setGravity(btVector3(gravity.x, gravity.y, gravity.z));
-///*
-//#ifdef _DEBUG
-//	mDebugDrawer_ = new OgreDebugDrawer(OgreContext::getInstance()->getSceneManager());
-//	mDebugDrawer_->setDebugMode(btIDebugDraw::DBG_DrawWireframe);
-//	dynamicsWorld->setDebugDrawer(mDebugDrawer_);
-//#endif // DEBUG
-//*/
-//}
+#include "PhysicsManager.h"
+
+
+#include <btBulletCollisionCommon.h>
+#include <btBulletDynamicsCommon.h>
+
+#include "Vector3D.h"
+
+PhysicsManager* PhysicsManager::_instance = nullptr;
+PhysicsManager* PhysicsManager::getInstance()
+{
+	return _instance;
+}
+
+bool PhysicsManager::setUpInstance() {
+	if (_instance == nullptr) {
+		try {
+			_instance = new PhysicsManager();
+			_instance->init(Vector3D(0.0, -9.8, 0.0));
+		}
+		catch (...) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+PhysicsManager::PhysicsManager(){
+	//registerComponent("RigidBody", 0, []() -> RigidBody * { return new RigidBody(); });
+
+}
+PhysicsManager::~PhysicsManager()
+{
+}
+;
+
+void PhysicsManager::init(const Vector3D gravity)
+{
+	collConfig = new btDefaultCollisionConfiguration();
+
+	collDispatcher = new btCollisionDispatcher(collConfig);
+
+	broadPhaseInterface = new btDbvtBroadphase();
+	constraintSolver = new btSequentialImpulseConstraintSolver();
+
+	dynamicsWorld = new btDiscreteDynamicsWorld(collDispatcher, broadPhaseInterface,
+		constraintSolver, collConfig);
+
+	dynamicsWorld->setGravity(btVector3(gravity.getX(), gravity.getY(), gravity.getZ()));
+/*
+#ifdef _DEBUG
+	mDebugDrawer_ = new OgreDebugDrawer(OgreContext::getInstance()->getSceneManager());
+	mDebugDrawer_->setDebugMode(btIDebugDraw::DBG_DrawWireframe);
+	dynamicsWorld->setDebugDrawer(mDebugDrawer_);
+#endif // DEBUG
+*/
+}
--- VERNIERENGINE/Src/PhysicsEngine/PhysicsManager.h
+++ VERNIERENGINE/Src/PhysicsEngine/PhysicsManager.h
@@ -1,48 +1,59 @@
-//#pragma once
-//#include <map>
-//
-//class btDefaultCollisionConfiguration;
-//class btCollisionDispatcher;
-//class btBroadphaseInterface;
-//class btSequentialImpulseConstraintSolver;
-//class btDiscreteDynamicsWorld;
-//class btRigidBody;
-//class OgreDebugDrawer;
-//class Vector3;
-//class CollisionObject;
-//class btCollisionObject;
-//
-//class PhysicsManager {
-//public:
-//	static PhysicsManager* getInstance();
-//	static bool setUpInstance();
-//
-//	//inicializa todas las variables fisicas asi como el "mundo" a partir de dichas variables
-//	void init(const Vector3 gravity);
-//private:
-//	PhysicsManager();
-//	virtual ~PhysicsManager();
-//	static PhysicsManager* _instance;
-//
-//	//cosas de bullet
-//	//Configuracion sobre la gestion de colisiones con bullet, nosotros usaremos la configuracion por defecto
-//	btDefaultCollisionConfiguration* collConfig = nullptr;
-//
-//	//Variable de bullet que hace de "pasador" de colisiones
-//	btCollisionDispatcher* collDispatcher = nullptr;
-//
-//	//Variable de bullet que se usa para hacer calculos de manera eficiente para generar posibles colisiones
-//	btBroadphaseInterface* broadPhaseInterface = nullptr;
-//
-//	//Variable de bullet que hace de solucionador de restricciones 
-//	btSequentialImpulseConstraintSolver* constraintSolver = nullptr;
-//
-//	//Variable de bullet a la que se le pasa todas las variables anteriores como configuracion de la fisica
-//	btDiscreteDynamicsWorld* dynamicsWorld = nullptr;
-//
-//	//Esto seria para dibujar los colliders en un modo debug, lo queremos?
-//	OgreDebugDrawer* mDebugDrawer_ = nullptr;
-//
-//	std::map<const btCollisionObject*, std::pair<CollisionObject*, CollisionObject*>> contacts;
-//
-//};
+#pragma once
+#include <map>
+#include <vector>
+
+class btDefaultCollisionConfiguration;
+class btCollisionDispatcher;
+class btBroadphaseInterface;
+class btSequentialImpulseConstraintSolver;
+class btDiscreteDynamicsWorld;
+class btRigidBody;
+class OgreDebugDrawer;
+class Vector3D;
+class CollisionObject;
+class btCollisionObject;
+class btCollisionShape;
+
+class PhysicsManager {
+public:
+	static PhysicsManager* getInstance();
+	static bool setUpInstance();
+
+	btDiscreteDynamicsWorld* getDynamicsWorld() { return dynamicsWorld; }
+	std::vector<btCollisionShape*> getCollisionShapes() { return collisionShapes; }
+	//void trackRigidBodyWithName(btRigidBody* body, std::string physicsCubeName) { physicsAccessors.insert({ std::string("cube"), body }); }
+	// 
+	// 
+	// 
+	//inicializa todas las variables fisicas asi como el "mundo" a partir de dichas variables
+	void init(const Vector3D gravity);
+private:
+	PhysicsManager();
+	virtual ~PhysicsManager();
+	static PhysicsManager* _instance;
+
+	//cosas de bullet
+	//Configuracion sobre la gestion de colisiones con bullet, nosotros usaremos la configuracion por defecto
+	btDefaultCollisionConfiguration* collConfig = nullptr;
+
+	//Variable de bullet que hace de "pasador" de colisiones
+	btCollisionDispatcher* collDispatcher = nullptr;
+
+	//Variable de bullet que se usa para hacer calculos de manera eficiente para generar posibles colisiones
+	btBroadphaseInterface* broadPhaseInterface = nullptr;
+
+	//Variable de bullet que hace de solucionador de restricciones 
+	btSequentialImpulseConstraintSolver* constraintSolver = nullptr;
+
+	//Variable de bullet a la que se le pasa todas las variables anteriores como configuracion de la fisica
+	btDiscreteDynamicsWorld* dynamicsWorld = nullptr;
+
+	//Esto seria para dibujar los colliders en un modo debug, lo queremos?
+	OgreDebugDrawer* mDebugDrawer_ = nullptr;
+
+
+	std::vector<btCollisionShape*> collisionShapes;
+
+	std::map<const btCollisionObject*, std::pair<CollisionObject*, CollisionObject*>> contacts;
+
+};
