8f27cab8a37d20c158b681cf02f0bc80eef3900d
simekons <simekons@ucm.es>
2022-04-24 14:15:47 +0200

    Integrado SDL_image.h

--- VERNIERENGINE/Src/Common/Button1.cpp
+++ VERNIERENGINE/Src/Common/Button1.cpp
@@ -1,11 +1,1 @@
 #pragma once
-
-#include "Vector3D.h"
-
-class Collisions {
-public:
-	static bool collides( const Vector3D o1Pos, float o1Width, float o1Height, const Vector3D o2Pos, float o2Width, float o2Height);
-
-private:
-	Collisions() = delete;
-};
