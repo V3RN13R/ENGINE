d89efd63afd4db040186cf76641b62b5fb4388d6
Alejao03 <alejao03@ucm.es>
2022-05-03 17:37:22 +0200

    Borrado juego del motor
    jaja´nt nos quedamos sin pruebas en motor
    
    Co-Authored-By: Agusmoure <48672414+Agusmoure@users.noreply.github.com>
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>

--- VERNIERENGINE/Src/Common/InitFactories.cpp
+++ VERNIERENGINE/Src/Common/InitFactories.cpp
@@ -4,49 +4,32 @@
 #include "MeshRenderer.h"
 #include "Transform.h"
 #include "RigidBody.h"
-#include "Shoot.h"
 #include "Camera.h"
 #include "Light.h"
-#include "MovementPlayer.h"
-#include "MonkeyStats.h"
-#include "Health.h"
-#include "Ground.h"
-#include "Pickable.h"
-#include "Player.h"
 #include "Image.h"
 #include "Text.h"
-#include "BananaMovement.h"
 #include "SoundComponent.h"
-#include "Destroyable.h"
 #include <ENGINE.h>
 //#include "checkML.h"
 
 Component* TransformFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new Transform(args);
+	return new Transform(args);
 };
-
 
 Component* MeshRendererFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new MeshRenderer(args);
+	return new MeshRenderer(args);
 };
-
 
 Component* RigidBodyFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new Rigidbody(args);
-};
-
-
-Component* ShootFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return _c = new Shoot(args, ent);
+	return new Rigidbody(args);
 };
 
 Component* CameraFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new Camera(args, ent);
+	return new Camera(args, ent);
 }
 
 //Component* ImageFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
@@ -54,29 +37,19 @@ Component* CameraFactory::createComponent(std::map<std::string, std::string> arg
 //	return new Image(args, ent);
 //};
 
-Component* MovementPlayerFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
+Component* SoundComponentFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new MovementPlayer(args);
-};
-
-Component* PickableFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return _c = new Pickable(args);
-};
-
-Component* GroundFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return _c = new Ground(args);
-};
+	return new SoundComponent(args, ent);
+}
 
 Component* Light_Factory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new Light(args, ent);
+	return new Light(args, ent);
 };
 
 Component* ImageFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
 {
-	return _c = new Image(args);
+	return new Image(args);
 };
 
 Component* TextFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
@@ -84,41 +57,9 @@ Component* TextFactory::createComponent(std::map<std::string, std::string> args,
 	return new Text(args);
 };
 
-
-
-//Component* MonkeyStatsFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-//{
-//	return new MonkeyStats(args);
-//};
-
 //Component* Button1Factory::createComponent(std::map<std::string, std::string> args, Entity* ent) {
 //	return new Button1(args, ent);
 //}
-
-Component* HealthFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return _c = new Health(args);
-};
-
-Component* PlayerFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return _c = new Player(args);
-};
-
-Component* BananaMovementFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	_c = new BananaMovement(args, ent);
-	return (_c);
-}
-Component* SoundComponentFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return new SoundComponent(args, ent);
-}
-
-Component* DestroyableFactory::createComponent(std::map<std::string, std::string> args, Entity* ent)
-{
-	return new Destroyable(args);;
-}
 
 void setupFactories()
 {
@@ -127,21 +68,12 @@ void setupFactories()
 	_fM->addFactory("Transform", new TransformFactory());
 	_fM->addFactory("MeshRenderer", new MeshRendererFactory());
 	_fM->addFactory("Rigidbody", new RigidBodyFactory());
-	_fM->addFactory("Shoot", new ShootFactory());
 	_fM->addFactory("Camera", new CameraFactory());
-	_fM->addFactory("MovementPlayer", new MovementPlayerFactory());
 	_fM->addFactory("Image", new ImageFactory());
 	_fM->addFactory("Text", new TextFactory());
 	_fM->addFactory("Light", new Light_Factory());
-	_fM->addFactory("Health", new HealthFactory());
-	_fM->addFactory("Ground", new GroundFactory());
-	_fM->addFactory("Pickable", new PickableFactory());
-	_fM->addFactory("Player", new PlayerFactory());
-	_fM->addFactory("BananaMovement", new BananaMovementFactory());
 	_fM->addFactory("SoundComponent", new SoundComponentFactory());
-	_fM->addFactory("Destroyable", new DestroyableFactory());
 
-	//_fM->addFactory("MonkeyStats", new MonkeyStatsFactory());
 	//_fM->addFactory("Button1", new Button1Factory());
 }
 
--- VERNIERENGINE/Src/Common/InitFactories.h
+++ VERNIERENGINE/Src/Common/InitFactories.h
@@ -1,101 +1,38 @@
 #pragma once
 #include "Factory.h"
 #include <map>
+
 class Component;
+
 class TransformFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~TransformFactory() { if (_c != nullptr) delete _c; };
 };
 
 class MeshRendererFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~MeshRendererFactory() { if(_c != nullptr) delete _c; };
 };
 
 class RigidBodyFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~RigidBodyFactory() { if (_c != nullptr) delete _c; };
-};
-
-class ShootFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~ShootFactory() { if (_c != nullptr) delete _c; };
 };
 
 class CameraFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~CameraFactory() { if (_c != nullptr)delete _c; };
-};
-
-class MovementPlayerFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~MovementPlayerFactory() { if (_c != nullptr) delete _c; };
-};
-
-class GroundFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~GroundFactory() { if (_c != nullptr) delete _c; };
-};
-
-class PickableFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~PickableFactory() { if (_c != nullptr) delete _c; };
-};
-
-
-class PlayerFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~PlayerFactory() { if (_c != nullptr) delete _c; };
 };
 
 class ImageFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~ImageFactory() { if (_c != nullptr) delete _c; };
 };
-
-
-
-
 
 //class ImageFactory : public Factory
 //{
@@ -105,52 +42,17 @@ public:
 
 class Light_Factory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~Light_Factory() { if (_c != nullptr) delete _c; };
-};
-
-//class MonkeyStatsFactory : public Factory
-//{
-//public:
-//	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-//};
-
-class HealthFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~HealthFactory() { if (_c != nullptr) delete _c; };
 };
 
 class TextFactory : public Factory
 {
-private:
-	Component* _c;
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~TextFactory() { if (_c != nullptr)delete _c; };
 };
 
-class BananaMovementFactory : public Factory
-{
-private:
-	Component* _c;
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-	//virtual ~BananaMovementFactory() { if (_c != nullptr) delete _c; };
-};
 class SoundComponentFactory : public Factory
-{
-public:
-	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
-};
-
-class DestroyableFactory : public Factory
 {
 public:
 	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
@@ -160,4 +62,5 @@ public:
 //{
 //	Component* createComponent(std::map<std::string, std::string> args, Entity* ent) override;
 //};
+
 void setupFactories();
