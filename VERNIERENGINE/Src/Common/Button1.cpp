cc1fe79369d11f18d01e7e761d49be33857d9c04
Alejao03 <alejao03@ucm.es>
2022-05-03 19:33:07 +0200

    Resubido componente Button1
    Co-Authored-By: Agusmoure <48672414+Agusmoure@users.noreply.github.com>
    Co-Authored-By: albgom21 <62644841+albgom21@users.noreply.github.com>

---
+++ VERNIERENGINE/Src/Common/Button1.cpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Button1.h"
+#include "InputManager.h"
+#include "Entity.h"
+#include "Transform.h"
+#include "Collisions.h"
+#include "Callbacks.h"
+
+Button1::Button1(std::map<std::string, std::string> args, Entity* ent) : _callbackName(args["callback"]) {
+    _ent = ent;
+}
+
+void Button1::start() {
+    _tr = static_cast<Transform*>(_ent->getComponent("Transform"));
+
+    setCallBackFunction(_callbackName);
+}
+
+void Button1::update() {
+    // Cogemos las coordenadas del rat�n.
+    float mX = InputManager::getInstance()->getMousePos().first;
+    float mY = InputManager::getInstance()->getMousePos().second;
+
+    // Creamos el Vector3D con las coordenadas del rat�n.
+    Vector3D vRaton(mX, mY, 0);
+    //std::cout << vRaton.getX() << " " << vRaton.getY() << std::endl;
+
+    // Detectamos el click en la zona.
+    if (Collisions::collides(vRaton, 10, 10, _tr->getPos(), 400, 400) && InputManager::getInstance()->getMouseDown()) {
+        std::cout << "COLISIONES" << std::endl;
+    }
+}
+
+void Button1::setCallBackFunction(std::string callBackName)
+{
+    setCallBackFunction(Callbacks::instance()->getMethod(callBackName));
+}
+
+void Button1::setCallBackFunction(std::function<void()> callBack)
+{
+    //element_->removeAllEvents();
+    //element_->subscribeEvent(CEGUI::PushButton::EventClicked, callBack);
+}
