014fd88e407c7a27f7f09387d55b74d0ba613e3b
Agusmoure <amoure@ucm.es>
2022-05-03 23:14:21 +0200

    Pequeño cambio para que sea más fácil debuggar

--- VERNIERENGINE/Exes/Data.lua
+++ VERNIERENGINE/Exes/Data.lua
@@ -1,7 +1,7 @@
 --DATA
 -- Insert the name of the DLL without de extension Example if the DLL generated is ExampleDLL.dll then write ExampleDLL
 function DLLName()
- return "DllExample_d"
+ return "DllExample"
 end
 -- Insert the name of the function that start the game, remember this function must not be a part of a class and it has to be writing as a extern C
 --If the function is the next
