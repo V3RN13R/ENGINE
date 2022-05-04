739eafa4e319db865b2eadeed0df74a716db4d11
albgom21 <albgom21@ucm.es>
2022-05-04 19:25:31 +0200

    Arreglado BAT
    Co-Authored-By: Alejao03 <61582507+Alejao03@users.noreply.github.com>
    Co-Authored-By: David Carmona Fauste <37439085+DavidCarmonaFauste@users.noreply.github.com>
    Co-Authored-By: madelino22 <62880766+madelino22@users.noreply.github.com>

--- VernierEngine.bat
+++ VernierEngine.bat
@@ -6,6 +6,6 @@ cd .\VERNIERENGINE\Dependencies
 call  Dependecies.bat
 
 @REM :: Una vez compiladas las dependencias pasamos a compilar nuestro motor como tal 
-cd ..\VERNIERENGINE
+cd ..
 msbuild "VERNIERENGINE.sln" /p:configuration=Debug /p:platform=x64
 msbuild "VERNIERENGINE.sln" /p:configuration=Release /p:platform=x64
